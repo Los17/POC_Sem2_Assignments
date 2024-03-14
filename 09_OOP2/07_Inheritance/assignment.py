34548ba950675333a9691096e87a841efda67a32
Carlos Lopez <lopezcar000@warren.k12.in.us>
2024-03-14 09:59:38 -0400

    neww

--- 09_OOP2/07_Inheritance/assignment.py
+++ 09_OOP2/07_Inheritance/assignment.py
@@ -1,5 +1,6 @@
 
 class Rectangle:
+    
     def __init__(self, base: float, height: float) -> None:
         if (base < 0):
             self.__base = 0
@@ -9,12 +10,18 @@ class Rectangle:
             self.__height = 0
         else:
             self.__height = height
+   
     def get_height(self) -> float:
         return self.__height
-    # YOUDO the get_base method
+    
+    def get_base(self) -> float:
+        return self.__base
+
     def get_perimeter(self) -> float:
         return 2 * self.__base + 2 * self.__height
-    # Youdo get_area method
+   
+    def get_area(self) -> float:
+        return self.__base * self.__height
 
     def __str__(self) -> str:
         # Rectangle of base:3, height:4
