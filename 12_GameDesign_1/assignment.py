165f2ea6152ee03d772e56fd64a7dae17f1c95c1
Carlos Lopez <lopezcar000@warren.k12.in.us>
2024-04-09 09:54:51 -0400

    12

---
+++ 12_GameDesign_1/assignment.py
@@ -0,0 +1,31 @@
+grid = [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"],
+]
+
+def print_grid():
+    for row in range(len(grid[row])):
+        for col in range(row):
+            print(grid[row][col], end="")
+            if (col != 2 ):
+                print("|", end="")
+        print()
+        if(row != 2):
+            print("*****")
+        
+        
+
+def game_loop():
+    print("Welcome to tic tac toe")
+    while(True):
+        user_request = input("Enter STOP to end or anything else to continue!")
+        if user_request.__eq__("STOP"):
+            break
+        print("Loopis runnning")
+    print("GAME IS OVER!")
+
+
+
+
+game_loop()
